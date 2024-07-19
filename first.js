8114d502dd338b1eee35a0bf1ed63e28e7f98d48
Shaik-Wajahat-Ali-Khan <aliwajahatkhan345@gmail.com>
2024-07-19 17:08:52 +0530

    Basic Program Prac

--- first.js
+++ first.js
@@ -1,4 +1,82 @@
-function A(x,y,z){
-    console.log(x-y+z)
+//Arithematic operations:-
+//function A(x,y,z){
+   // console.log(x-y+z)
+//}
+//A(10,20,30)
+
+//Exponential:-
+ //let x=[1,2,3,4,5,6]
+//x[3]=3**2;
+//var y=0
+//for(i=0;i<x.length;i++){
+   // y=y+x[i]
+//}
+//console.log(y)
+
+//In Array the number 2 how many times are repeated we are Finding:- 
+//let x=[2,3,5,2,8,5,9];
+ //var y=0;
+//for(i=0;i<x.length;i++){
+   // if(x[i]===2){
+    //    y++;
+    //}
+//}
+//console.log("The Number 2 Appears",y,"times in the array.");
+
+//creating variables:-
+//var y=20;
+//const z=9;
+//{
+    //var x=10;
+//}
+//console.log(x);
+
+//Post increment:-
+/*let x=10;
+console.log("x",x)
+x++;
+console.log("x",x)*/
+
+//Pre increment:-
+/*let y=20;
+//console.log("y",y)
+++y;
+console.log("y",y)*/
+
+//Forloop statements:-
+/*for(let i=0;i<4;i++){
+console.log("ALI");
+}*/
+
+/*for(i=0;i<4;i++){
+    for(j=0;j<2;j++){
+        console.log("ALI")
+    }
+}*/
+
+/*let x=0;
+while(x<4){
+    console.log("KHAN")
+    x++;
+}*/
+
+/*let y=0;
+while(y<4){
+    for(i=0;i<2;i++){
+        console.log("WAJAHAT")
+    }
+    y++
+}*/
+
+//Conditional Statements with logical operators(&,||):-
+/*if(7>6&&9>10){
+    console.log("Right")
+}else{
+    console.log("Wrong")
+}*/
+
+if(7>6||9>10){
+    console.log("Right")
+}else{
+    console.log("Wrong")
 }
-A(10,20,30)
